d2f8ff2fc918614402df70e4c2999ba25f315142
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-04-05 19:24:13 +0100

    Create post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import {useState} from 'react'
 import {Header} from './components/Header'
-import {Post} from './Post'
+import {Post} from './components/Post'
+import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
-import { Sidebar } from './components/Sidebar';
 
 function App() {
     const [count, setCount] = useState(0)
@@ -21,13 +21,6 @@ function App() {
                         molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
                         numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
                         optio, eaque rerum! Provident similique accusantium nemo autem.'/>
-
-                    <Post
-                        author='Isaque Nobre'
-                        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                        optio, eaque rerum! Provident similique accusantium nemo autem.'/>      
                 </main>
 
             </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css';
+
+export const Post = () =>{
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' />
+                    <div className={styles.authorInfo}>
+                        <strong>Jordan Zanoni</strong>
+                        <span>Web Develober</span>
+                    </div>
+                </div>
+                <time title='04 de maio as 17:25h' dateTime='2023-05-04 17:25h'>Publicado ha 1h</time>
+            </header>
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+            <p>👉 <a href=''>jane.design/doctorcare</a></p>
+
+            <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+
+}
