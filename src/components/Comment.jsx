a45a3cca169ad48f451b9012bff1b48c24ca5ea0
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-04-06 18:59:53 +0100

    Create component Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { AlignCenterHorizontalSimple, ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+    <div className={styles.comment}>
+        <img src='https://avatars.githubusercontent.com/u/52969396?v=4' alt='' />
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                        <strong>Jordan Zanoni</strong>
+                        <time title='04 de maio as 17:25h' dateTime='2023-05-04 17:25h'>Cerca de 1h atras</time>
+                    </div>
+                    <button title='delete comment'>
+                        <Trash size={20} />
+                    </button>
+                </header>
+
+                <p>Muito bom Devon, parabéns!! 👏👏</p>
+            </div>
+            <footer>
+                <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </button>
+            </footer>
+        </div>
+    </div> );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export const Post = () =>{
@@ -21,6 +22,20 @@ export const Post = () =>{
             <p>👉 <a href=''>jane.design/doctorcare</a></p>
 
             <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu comentario</strong>
+                <textarea placeholder='Deixe um comentario'/>
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
